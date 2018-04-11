f260a568da77d0c2808c550d3e065dfb2fb99acd
alberturria <alberturria@users.noreply.github.com>
2018-04-11 18:10:21 +0200

    .

--- public/assests/js/googlemap.js
+++ public/assests/js/googlemap.js
@@ -1,7 +1,11 @@
-function myMap() {
-    var mapProp= {
-        center:new google.maps.LatLng(51.508742,-0.120850),
-        zoom:5,
-    };
-    var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
-    }
+function initMap() {
+    var milano = {lat: 45.477987, lng: 9.226353};
+    var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 15,
+    center: milano
+    });
+    var marker = new google.maps.Marker({
+    position: milano,
+    map: map
+    });
+}
