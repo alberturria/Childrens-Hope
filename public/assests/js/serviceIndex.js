36b2784fb7444851bbf025dc65cd2f8ef8631de4
alberturria <alberturria@users.noreply.github.com>
2018-05-08 23:55:35 +0200

    enlazar Servicios

--- public/assests/js/serviceIndex.js
+++ public/assests/js/serviceIndex.js
@@ -3,8 +3,8 @@ var services = [
     // number of people
     // Name      image       description
     ["Canteen", "../assests/img/milan.jpg", "A brief description"],
-    ["Cafeteria", "../assests/img/bologna.jpg", "A brief description"],
-    ["Naples", "../assests/img/naples.jpg", "A brief description"],
+    ["Cafe", "../assests/img/bologna.jpg", "A brief description"],
+    ["Parking", "../assests/img/naples.jpg", "A brief description"],
     ["Rome", "../assests/img/rome.jpg", "A brief description"]
     ];
     
@@ -13,21 +13,21 @@ var services = [
     });
     
     
-    function getQueryVariable(variable) {
+    function getQueryVariable() {
         var query = document.URL;
         var vars = query.split("=");
         return vars[1];
     }
     
     function loadServiceDescription(index){
-        document.getElementById("serviceTittle").innerHTML= getQueryVariable(location);
-        var current = locations;
-        for(var i=0; i < locations.length; i++)
+        document.getElementById("serviceTittle").innerHTML= getQueryVariable();
+        var current = services;
+        for(var i=0; i < services.length; i++)
         {
-            if(getQueryVariable(location)==locations[i][0])
+            if(getQueryVariable()==services[i][0])
             {
-                document.getElementById("serviceDescription").innerHTML= locations[i][2];   
-                document.getElementById("serviceImage").src=locations[i][1];
+                document.getElementById("serviceDescription").innerHTML= services[i][2];   
+                document.getElementById("serviceImage").src=services[i][1];
             }
         }
     }
