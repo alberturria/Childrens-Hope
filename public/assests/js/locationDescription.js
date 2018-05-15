221fdc96066774179b8e26efd03ce71d77c382d0
Paco <pacococacruz@gmail.com>
2018-05-15 20:44:23 +0200

    relation between service and location in location description done

--- public/assests/js/locationDescription.js
+++ public/assests/js/locationDescription.js
@@ -7,29 +7,60 @@ var locations = [
 ["Rome", "../assests/img/rome.jpg", "The capital of Italy is one of the locations decided to place one of our centers. We have a small center placed in the outskirts of the city. It is easy to access with car. Find us in Via Capraia 2."]
 ];
 
+// simulate the relation beetwen services and location
+var relation = [
+["0","1","3","4","5"], // Milano
+["4","5"],  // Bologna
+["0","1","4"],  // Rome
+["0","1","2","3"]  // Naples
+];
+
+var services = [
+// id     Name
+["0", "Canteen"],
+["1", "Cafe"],
+["2", "Parking"],
+["3", "Rehabilitation"],
+["4", "Playroom"],
+["5", "Education"]
+];
+
 $(document).ready(function(){
-    loadlocationDescription(1);
+	loadlocationDescription(1);
 });
 
 
 function getQueryVariable() {
-    var query = document.URL;
-    var vars = query.split("=");
-    return vars[1];
+	var query = document.URL;
+	var vars = query.split("=");
+	return vars[1];
 }
 
-function loadlocationDescription(index){
-    document.getElementById("locationTittle").innerHTML= getQueryVariable();
-    var current = locations;
-    for(var i=0; i < locations.length; i++)
-    {
-        if(getQueryVariable()==locations[i][0])
-        {
-            document.getElementById("locationDescription").innerHTML= locations[i][2];   
-            document.getElementById("locationImg").src=locations[i][1];
-        }
-    }
+function loadlocationDescription(){
+	var location = getQueryVariable();
+	document.getElementById("locationTittle").innerHTML=location;
+	var city;
+
+	if (location=="Milano"){
+		city=0;
+	}else if (location=="Bologna"){
+		city=1;
+	}else if (location=="Rome"){
+		city=2;
+	}else{ // Naples
+		city=3;
+	}
+
+	citySer=relation[city];
+	document.getElementById("locationDescription").innerText= locations[city][2];
+	document.getElementById("locationImg").src=locations[city][1];
+
+	var result="<ul>";
+	for(var i=0; i<citySer.length; i++)
+		result+='<li><a href="serviceDescription.html?service='+services[citySer[i]][1]+'">'+services[citySer[i]][1]+'</a></li>';	
+	result+="</ul>";
+
+	document.getElementById("locationLinks").innerHTML=result;
 }
 
 
-    
