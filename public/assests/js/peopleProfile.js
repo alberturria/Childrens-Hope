bfe8bd8d094176e8477a56a3d0ebefbc750ea6fb
JoaquinBuzzles96 <joakilm2@gmail.com>
2018-05-15 23:35:19 +0200

    personas añadidas

--- public/assests/js/peopleProfile.js
+++ public/assests/js/peopleProfile.js
@@ -5,14 +5,19 @@ var people = [
 ["1", "Pietro Baldi", "pietroBaldi.jpeg", "Counselor","I love to guide young people with different needs, and help them", "pBaldi@csHope.com"],
 ["2", "Lamberta Pérez", "lambertaPerez.jpeg", "Lawyer","I wanted a change in my life, I am happier now helping other people", "lPerez@csHope.com"],
 ["3", "Nadia Ferrari", "nadiaFerrari.jpg", "Psychiatrist","I studied medicine in 'Universidad de Granada'", "nFerrari@csHope.com"],
-["4", "Person5", "p5.png", "A brief description", "email@hotmail.com"],
-["5", "Person6", "p6.png", "A brief description", "email@hotmail.com"],
-["6", "Person7", "p7.png", "A brief description", "email@hotmail.com"],
-["7", "Person8", "p8.png", "A brief description", "email@hotmail.com"],
-["8", "Person9", "p9.png", "A brief description", "email@hotmail.com"],
-["9", "Person10", "p10.png", "A brief description", "email@hotmail.com"],
-["10", "Person11", "p11.png", "A brief description", "email@hotmail.com"],
-["11", "Person12", "p12.png", "A brief description", "email@hotmail.com"]
+["4", "Yuri Esposito", "p5.png", "Security", "My dream has always been to defend the weak", "email@hotmail.com"],
+["5", "Isabella Ricci", "p6.png", "Teacher", "I want everyone to have access to education", "yEsposito@csHope.com"],
+["6", "Linda Romano", "p7.png", "Waiter", "I want to help the disadvantaged", "lRomano@csHope.com"],
+["7", "Antonella Greco", "p8.png", "Physiotherapist", "My dream is to help everyone", "aGreco@csHope.com"],
+["8", "Andrea Romanini", "p9.png", "Nurse", "Anything to see a smile", "aRomanini@csHope.com"],
+["9", "Lucca Colombetti", "p10.png", "Administrative", "My passion is the paperwork", "lColombetti@csHope.com"],
+["10", "Giovanni Grieco", "p11.png", "Carer", "The laugh is the best medicine", "gGrieco@csHope.com"],
+["11", "Angelina Marinazzo", "p12.png", "Waiter", "I want everyone to have the same opportunities", "aFerrari@csHope.com"]
+["12", "Franchesca Romanatti", "p13.png", "Doctor", "Healing the world", "fRomanatti@csHope.com"]
+["13", "Benito Camela", "p14.png", "Security", "Always defending the weak", "bCamela@csHope.com"]
+["14", "Lia Grechi", "p15.png", "Teacher", "My dream is to teach everyone", "lGrechi@csHope.com"]
+["15", "Davide Colombo ", "p12.png", "Chef", "Cooking for future hopes", "dColombo@csHope.com"]
+
 ];
 
 $(document).ready(function(){
