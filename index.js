a3c77cd9739964d6e10b784c66947fbf1fee4860
Paco <francisco.coca@email.polimi.it>
2018-05-27 20:17:54 +0200

    .

--- index.js
+++ index.js
@@ -7,52 +7,50 @@ const process = require("process");
 let sqlDb;
 
 function initSqlDB() {
-  /* Locally we should launch the app with TEST=true to use SQLlite:
-
-       > TEST=true node ./index.js
-
-    */
-  if (process.env.TEST) {
-    sqlDb = sqlDbFactory({
-      client: "sqlite3",
-      debug: true,
-      connection: {
-        filename: "./petsdb.sqlite"
-      },
-      useNullAsDefault: true
-    });
-  } else {
-    sqlDb = sqlDbFactory({
-      debug: true,
-      client: "pg",
-      connection: process.env.DATABASE_URL,
-      ssl: true
-    });
-  }
+	/* Locally we should launch the app with TEST=true to use SQLlite:
+		> TEST=true node ./index.js
+	*/
+	if (process.env.TEST) {
+		sqlDb = sqlDbFactory({
+			client: "sqlite3",
+			debug: true,
+			connection: {
+			filename: "./petsdb.sqlite"
+			},
+			useNullAsDefault: true
+		});
+	} else {
+		sqlDb = sqlDbFactory({
+			debug: true,
+			client: "pg",
+			connection: process.env.DATABASE_URL,
+			ssl: true
+		});
+	}
 }
 
 function initDb() {
-  return sqlDb.schema.hasTable("pets").then(exists => {
-    if (!exists) {
-      sqlDb.schema
-        .createTable("pets", table => {
-          table.increments();
-          table.string("name");
-          table.integer("born").unsigned();
-          table.enum("tag", ["cat", "dog"]);
-        })
-        .then(() => {
-          return Promise.all(
-            _.map(petsList, p => {
-              delete p.id;
-              return sqlDb("pets").insert(p);
-            })
-          );
-        });
-    } else {
-      return true;
-    }
-  });
+	return sqlDb.schema.hasTable("pets").then(exists => {
+		if (!exists) {
+			sqlDb.schema
+			.createTable("pets", table => {
+				table.increments();
+				table.string("name");
+				table.integer("born").unsigned();
+				table.enum("tag", ["cat", "dog"]);
+			})
+			.then(() => {
+				return Promise.all(
+				_.map(petsList, p => {
+					delete p.id;
+					return sqlDb("pets").insert(p);
+				})
+				);
+			});
+		} else {
+			return true;
+		}
+	});
 }
 
 const _ = require("lodash");
@@ -68,47 +66,47 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // /* Register REST entry point */
 app.get("/pets", function(req, res) {
-  let start = parseInt(_.get(req, "query.start", 0));
-  let limit = parseInt(_.get(req, "query.limit", 5));
-  let sortby = _.get(req, "query.sort", "none");
-  let myQuery = sqlDb("pets");
+	let start = parseInt(_.get(req, "query.start", 0));
+	let limit = parseInt(_.get(req, "query.limit", 5));
+	let sortby = _.get(req, "query.sort", "none");
+	let myQuery = sqlDb("pets");
 
-  if (sortby === "age") {
-    myQuery = myQuery.orderBy("born", "asc");
-  } else if (sortby === "-age") {
-    myQuery = myQuery.orderBy("born", "desc");
-  }
-  myQuery
-    .limit(limit)
-    .offset(start)
-    .then(result => {
-      res.send(JSON.stringify(result));
-    });
+	if (sortby === "age") {
+		myQuery = myQuery.orderBy("born", "asc");
+	} else if (sortby === "-age") {
+		myQuery = myQuery.orderBy("born", "desc");
+	}
+	myQuery
+	.limit(limit)
+	.offset(start)
+	.then(result => {
+		res.send(JSON.stringify(result));
+	});
 });
 
 app.delete("/pets/:id", function(req, res) {
-  let idn = parseInt(req.params.id);
-  sqlDb("pets")
-    .where("id", idn)
-    .del()
-    .then(() => {
-      res.status(200);
-      res.send({ message: "ok" });
-    });
+	let idn = parseInt(req.params.id);
+	sqlDb("pets")
+	.where("id", idn)
+	.del()
+	.then(() => {
+		res.status(200);
+		res.send({ message: "ok" });
+	});
 });
 
 app.post("/pets", function(req, res) {
-  let toappend = {
-    name: req.body.name,
-    tag: req.body.tag,
-    born: req.body.born
-  };
-  sqlDb("pets")
-    .insert(toappend)
-    .then(ids => {
-      let id = ids[0];
-      res.send(_.merge({ id, toappend }));
-    });
+	let toappend = {
+	name: req.body.name,
+	tag: req.body.tag,
+	born: req.body.born
+	};
+	sqlDb("pets")
+	.insert(toappend)
+	.then(ids => {
+		let id = ids[0];
+		res.send(_.merge({ id, toappend }));
+	});
 });
 
 // app.use(function(req, res) {
@@ -123,5 +121,5 @@ initDb();
 
 /* Start the server on port 3000 */
 app.listen(serverPort, function() {
-  console.log(`Your app is ready at port ${serverPort}`);
+	console.log(`Your app is ready at port ${serverPort}`);
 });
