ad92d9fea169cdd7a56ee30a46b0afe1caf9132a
Francisco Coca Cruz <francisco.coca@email.polimi.it>
2018-06-14 19:08:32 +0200

    changed event description aspect

--- public/assests/js/eventDescription.js
+++ public/assests/js/eventDescription.js
@@ -17,26 +17,23 @@ function loadEventDescription(){
 			return response.json();
 		})
 		.then(function(data) {
-			console.log(data);
-			data.map(addRow);
+			data.map(loadEvent);
 		});
-/*
-	citySer=relation[city];
-	document.getElementById("locationDescription").innerText= locations[city][2];
-	document.getElementById("locationImg").src=locations[city][1];
-
-	var result="<ul>";
-	for(var i=0; i<citySer.length; i++)
-		result+='<li><a href="serviceDescription.html?service='+services[citySer[i]][1]+'">'+services[citySer[i]][1]+'</a></li>';	
-	result+="</ul>";
-
-	document.getElementById("locationLinks").innerHTML=result;*/
 }
 
 
-function addRow(event) {
+function loadEvent(event) {
 	document.getElementById("eventDescription").innerText= event.description;
-	document.getElementById("eventImg").src="../assests/img/"+event.img;
-    document.getElementById("eventTittle").innerHTML=event.name;	
-    document.getElementById("eventDate").innerHTML=event.date;	
+	document.getElementById("eventImg").style.backgroundImage="url(../assests/img/"+event.img+")";
+    document.getElementById("eventImg").style.backgroundSize="contain";
+    document.getElementById("eventImg").style.backgroundRepeat="no-repeat";
+    document.getElementById("eventImg").style.backgroundPosition="center";
+    document.getElementById("eventTittle").innerHTML=event.name;
+	document.getElementById("eventDate").innerHTML =convertDateFormat(event.date);
+
+}
+
+function convertDateFormat(string) {
+  var date = string.split('-');
+  return date[2] + '/' + date[1] + '/' + date[0];
 }
